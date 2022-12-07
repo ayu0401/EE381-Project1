11d6085eddad88927698984c827703037d7e2ef6
Aaron Yu <48599146+ayu0401@users.noreply.github.com>
2022-12-06 21:35:38 -0800

    major update

--- Project 1.py
+++ Project 1.py
@@ -1,12 +1,44 @@
 import csv
 import numpy as np
 
+# this method is used so that the array representation of my csv file is not in scientific notation
+np.set_printoptions(suppress=True)
 # we are opening the csv file and reading it to extract the data
+# we extracted the column that contains the sales value
 file = 'Sales_01_20.csv'
-data = np.loadtxt(file, delimiter=',', skiprows=1, dtype=int, usecols=1)
-print(data)
+data1 = np.loadtxt(file, delimiter=',', skiprows=1, usecols=1)
 
 
+data2 = np.loadtxt(file, delimiter=',', skiprows=1)
+
+
+# calculate Mean & Std of each year and print it
+yearCount = 2001
+meanArr = []
+while(yearCount < 2021):
+    for row in data2:
+        if row[0] == yearCount:
+            meanArr.append(row[1])
+            continue
+    mean = np.mean(meanArr)
+    std = np.std(meanArr)
+    strStd = ("%.3f" % std)
+    strMean = ("%.3f" % mean)
+    print('Mean for year: ' + str(yearCount) + ' = ' + strMean)
+    print('Standard Dev for year: ' + str(yearCount) + ' = ' + strStd + '\n')
+    meanArr.clear()
+    yearCount += 1
+    continue
+
+
+
+
+
+
+
+
+
+# this project is the death of me
 
 
 
